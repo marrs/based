1b69fd4104394b4b4f045c43d63038cb51e4cda4
David Marrs <david@marrsbytes.com>
2025-01-12 21:44:35 +0000

    Fail on missing db filename

--- main.c
+++ main.c
@@ -1,14 +1,31 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <string.h>
 
 #include <ncurses.h>
+#include <sqlite3.h>
 
-int main()
+int main(int argc, char **argv)
 {
+    sqlite3 *db;
+
+    if (argc > 1) {
+        int err = sqlite3_open_v2(argv[1], &db, SQLITE_OPEN_READONLY, 0);
+        if (err) {
+            fprintf(stderr, "Error opening %s: %s\n", argv[1], sqlite3_errmsg(db));
+            goto exit;
+        }
+    } else {
+        fprintf(stderr, "Exiting: no sqlite database provided\n");
+            goto exit;
+    }
+
     char help_msg[255] = "Options are (q)uit and (e)dit.\n";
 	initscr();			/* Start curses mode 		  */
+
 	printw("Ncurses Test\n");	
+
 	printw(help_msg);
 	refresh();			/* Print it on to the real screen */
     char input_ch;
