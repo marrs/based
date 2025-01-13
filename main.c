2561f0f26048e56e48ec13ef434dae96852995db
David Marrs <david@marrsbytes.com>
2025-01-13 00:15:53 +0000

    Display user tables.

--- main.c
+++ main.c
@@ -6,28 +6,70 @@
 #include <ncurses.h>
 #include <sqlite3.h>
 
-void ui_show_tables(sqlite3 *db)
+int str_starts_with(char *str, const char *prefix)
+{
+    return (0 == strncmp(prefix, str, strlen(prefix)));
+}
+
+void ui_show_user_tables(sqlite3 *db)
 {
     sqlite3_stmt *stmt;
     int err = 0;
-    char sql[255] = "pragma table_list;";
+    char sql[255] = "select schema, name, type, ncol, wr, strict "
+                    "from pragma_table_list;";
     err = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
     if (err) {
         printw("Error preparing statement: %s\n", sqlite3_errmsg(db));
     } else {
-        err = sqlite3_step(stmt);
-        switch (err) {
-            case SQLITE_DONE: {
-                printw("Success: Done.\n");
-            } break;
-            case SQLITE_ROW: {
-                printw("Success: Row returned.\n");
-            } break;
-            case SQLITE_MISUSE: {
-                printw("Error (SQLITE_MISUSE) stepping through statement: %s\n", sqlite3_errmsg(db));
-            } break;
-            default: {
-                printw("Error (%d) stepping through statement: %s\n", err, sqlite3_errmsg(db));
+        int cols = sqlite3_column_count(stmt);
+        printw("col count: %d\n", cols);
+        for (int idx = 0; idx < cols; ++idx) {
+            printw("  %s ", sqlite3_column_name(stmt, idx));
+        }
+        printw("\n");
+
+        while (err = sqlite3_step(stmt)) {
+            if (SQLITE_ROW == err) {
+                int col_type = 0;
+                char *table_name = sqlite3_column_text(stmt, 1);
+                if (!str_starts_with(table_name, "sqlite_")) {
+                    for (int idx = 0; idx < cols; ++idx) {
+                        switch (sqlite3_column_type(stmt, idx)) {
+                            case SQLITE_INTEGER:
+                                printw("  %d", sqlite3_column_int(stmt, idx));
+                                break;
+                            case SQLITE_FLOAT:
+                                printw("  %f", sqlite3_column_double(stmt, idx));
+                                break;
+                            case SQLITE_TEXT: {
+                                char *text = sqlite3_column_text(stmt, idx);
+                                if (!str_starts_with(text, "sqlite_")) {
+                                    printw("  %s", text);
+                                }
+                            } break;
+                            case SQLITE_BLOB:
+                                printw("  %f", sqlite3_column_blob(stmt, idx));
+                                break;
+                            case SQLITE_NULL:
+                                printw("  NULL");
+                                break;
+                            default:
+                                printw("  Unknown Type\n");
+                        }
+                    }
+                    printw("\n");
+                }
+            } else {
+                switch (err) {
+                case SQLITE_ROW: printw("Success: Row returned.\n"); break;
+                case SQLITE_DONE: printw("Success: Done.\n"); break;
+                case SQLITE_MISUSE:
+                    printw("Error (SQLITE_MISUSE) stepping through statement: %s\n", sqlite3_errmsg(db));
+                    break;
+                default:
+                    printw("Error (%d) stepping through statement: %s\n", err, sqlite3_errmsg(db));
+                }
+                break;
             }
         }
     }
@@ -55,7 +97,7 @@ int main(int argc, char **argv)
 	initscr();          /* Start curses mode 		  */
 
     printw("Ncurses Test\n");
-    ui_show_tables(db);
+    ui_show_user_tables(db);
 
     printw(help_msg);
     refresh();          /* Print it on to the real screen */
