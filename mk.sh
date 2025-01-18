9bd6699544fdac97bd4c015c6192ce24a36c3ec4
David Marrs <david@marrsbytes.com>
2025-01-18 19:30:23 +0000

    Add libcymal to mk.sh

--- mk.sh
+++ mk.sh
@@ -11,7 +11,9 @@ src="${1:-src/main.c}"
 
 gcc $(pkg-config --cflags sqlite3) \
     $(pkg-config --cflags ncurses) \
+    $(pkg-config --cflags libcyaml) \
     -g \
     -obin "$src" \
     $(pkg-config --libs ncurses) \
-    $(pkg-config --libs sqlite3)
+    $(pkg-config --libs sqlite3) \
+    $(pkg-config --libs libcyaml) \
